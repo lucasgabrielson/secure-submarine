8daf63f2dd6de07d4f6935a0c5958792e36775e7
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-04-13 16:46:31 -0500

    base mode complete

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -10,32 +10,13 @@ const router = express.Router();
 router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
-  if( req.user.clearance_level < 13 ) {
     pool
-    .query(`SELECT * FROM "secret" WHERE secrecy_level < 13;`)
+    .query(`SELECT * FROM "secret" WHERE secrecy_level < ${req.user.clearance_level};`)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
       res.sendStatus(500);
-    });
-  }
-  if( req.user.clearance_level < 6 ) {
-    pool
-    .query(`SELECT * FROM "secret" WHERE secrecy_level < 6;`)
-    .then((results) => res.send(results.rows))
-    .catch((error) => {
-      console.log('Error making SELECT for secrets:', error);
-      res.sendStatus(500);
-    });
-  } else {
-    pool
-    .query(`SELECT * FROM "secret";`)
-    .then((results) => res.send(results.rows))
-    .catch((error) => {
-      console.log('Error making SELECT for secrets:', error);
-      res.sendStatus(500);
-    });
-  }  
+    }); 
 });
 
 module.exports = router;
